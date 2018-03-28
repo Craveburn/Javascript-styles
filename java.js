88c17787730fdb3f0d5417083bdb4bc1218f2555
Cameron Rabunn <raburncameron@gmail.com>
2018-03-27 18:15:09 -0600

    this was for the assignment

---
+++ java.js
@@ -0,0 +1,16 @@
+// For this assignment you'll need to concatenate (add together) your first name and last 
+// name into a string.  Assign the result of the string concatenation to a variable:
+let first = 'Cameron ';
+let last = 'Raburn';
+let fullName = first + last;
+
+// Add two numbers together, and assign the result in a variable: calculation
+let a = 43;
+let b = 42;
+let c = b + a;
+
+//Add a number and a string together. Store the result in a variable: unexpected
+let a = 'cameron is ';
+let b = 24;
+let c = a + b;
+
